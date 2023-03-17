97b41d8eb945d348d70e1042f22bc8a037d56cb2
Rizvan555 <rizvan84@gmx.de>
2023-03-17 17:39:26 +0100

    added result to center

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,5 +1,9 @@
 const unserWindow = document.querySelector(".user-window");
 const compWindow = document.querySelector(".comp-window");
+const count5 = document.querySelector(".count-5");
+const count10 = document.querySelector(".count-10");
+const count15 = document.querySelector(".count-15");
+const count20 = document.querySelector(".count-20");
 const userCountDisplay = document.querySelector(".user-count");
 const compCountDisplay = document.querySelector(".comp-count");
 const startCommand = document.querySelector(".start-command");
@@ -7,9 +11,7 @@ const stoneButton = document.querySelector(".stone");
 const paperButton = document.querySelector(".paper");
 const shearsButton = document.querySelector(".shears");
 const restartButton = document.querySelector(".restart");
-// const roundsForm = document.querySelector(".rounds-section");
-// const userCompSection = document.querySelector(".user-comp__section");
-// const buttonContainer = document.querySelector(".button-container");
+
 let playerScore = 0;
 let computerScore = 0;
 const moves = ["stone", "paper", "shears"];
@@ -17,36 +19,60 @@ unserWindow.innerHTML = playerScore;
 compWindow.innerHTML = computerScore;
 
 function playRound(playerSelection, computerSelection) {
-  if (playerSelection === computerSelection) {
-    return "tie";
-  } else if (
-    (playerSelection === "stone" && computerSelection === "shears") ||
-    (playerSelection === "paper" && computerSelection === "stone") ||
-    (playerSelection === "shears" && computerSelection === "paper")
-  ) {
-    playerScore++;
-    return "win";
-  } else {
-    computerScore++;
-    return "lose";
+  for (let index = 0; index <= 5; index++) {
+    if (playerSelection === computerSelection) {
+      return "tie";
+    } else if (
+      (playerSelection === "stone" && computerSelection === "shears") ||
+      (playerSelection === "paper" && computerSelection === "stone") ||
+      (playerSelection === "shears" && computerSelection === "paper")
+    ) {
+      playerScore++;
+      return "win";
+    } else {
+      computerScore++;
+      return "lose";
+    }
   }
 }
 
-const startCommands = () => {
-  roundsForm.classList.add("hidden");
-  userCompSection.classList.remove("hidden");
-  buttonContainer.classList.remove("hidden");
-};
+let round = 0;
 
 function makeSelection(selection) {
   const computerSelection = moves[Math.floor(Math.random() * 3)];
   const result = playRound(selection, computerSelection);
   unserWindow.innerHTML = selection;
   compWindow.innerHTML = computerSelection;
+  round++;
+
   if (result === "win") {
     userCountDisplay.innerHTML = parseInt(userCountDisplay.innerHTML) + 1;
   } else if (result === "lose") {
     compCountDisplay.innerHTML = parseInt(compCountDisplay.innerHTML) + 1;
+  }
+
+  if (round === 5) {
+    if (playerScore > computerScore) {
+      startCommand.innerHTML = "You win!";
+    } else if (computerScore > playerScore) {
+      startCommand.innerHTML = "Computer wins!";
+    } else {
+      startCommand.innerHTML = "It's a tie!";
+    }
+    stoneButton.disabled = true;
+    paperButton.disabled = true;
+    shearsButton.disabled = true;
+  } else if (round === 10) {
+    if (playerScore > computerScore) {
+      startCommand.innerHTML = "You win!";
+    } else if (computerScore > playerScore) {
+      startCommand.innerHTML = "Computer wins!";
+    } else {
+      startCommand.innerHTML = "It's a tie!";
+    }
+    stoneButton.disabled = true;
+    paperButton.disabled = true;
+    shearsButton.disabled = true;
   }
 }
 const clickStone = () => {

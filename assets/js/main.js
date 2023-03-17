86cd9f4d4ecdae5fe53a765e423edea9bc152957
Rizvan555 <rizvan84@gmx.de>
2023-03-17 17:01:01 +0100

    changed addEventListener with function

--- assets/js/main.js
+++ assets/js/main.js
@@ -7,6 +7,9 @@ const stoneButton = document.querySelector(".stone");
 const paperButton = document.querySelector(".paper");
 const shearsButton = document.querySelector(".shears");
 const restartButton = document.querySelector(".restart");
+// const roundsForm = document.querySelector(".rounds-section");
+// const userCompSection = document.querySelector(".user-comp__section");
+// const buttonContainer = document.querySelector(".button-container");
 let playerScore = 0;
 let computerScore = 0;
 const moves = ["stone", "paper", "shears"];
@@ -29,50 +32,32 @@ function playRound(playerSelection, computerSelection) {
   }
 }
 
-startCommand.addEventListener("click", function () {
-  const roundsForm = document.querySelector(".rounds-section");
-  const userCompSection = document.querySelector(".user-comp__section");
-  const buttonContainer = document.querySelector(".button-container");
+const startCommands = () => {
   roundsForm.classList.add("hidden");
   userCompSection.classList.remove("hidden");
   buttonContainer.classList.remove("hidden");
-});
+};
 
-stoneButton.addEventListener("click", function () {
+function makeSelection(selection) {
   const computerSelection = moves[Math.floor(Math.random() * 3)];
-  const result = playRound("stone", computerSelection);
-  unserWindow.innerHTML = "stone";
+  const result = playRound(selection, computerSelection);
+  unserWindow.innerHTML = selection;
   compWindow.innerHTML = computerSelection;
   if (result === "win") {
     userCountDisplay.innerHTML = parseInt(userCountDisplay.innerHTML) + 1;
   } else if (result === "lose") {
     compCountDisplay.innerHTML = parseInt(compCountDisplay.innerHTML) + 1;
   }
-});
-
-paperButton.addEventListener("click", function () {
-  const computerSelection = moves[Math.floor(Math.random() * 3)];
-  const result = playRound("paper", computerSelection);
-  unserWindow.innerHTML = "paper";
-  compWindow.innerHTML = computerSelection;
-  if (result === "win") {
-    userCountDisplay.innerHTML = parseInt(userCountDisplay.innerHTML) + 1;
-  } else if (result === "lose") {
-    compCountDisplay.innerHTML = parseInt(compCountDisplay.innerHTML) + 1;
-  }
-});
-
-shearsButton.addEventListener("click", function () {
-  const computerSelection = moves[Math.floor(Math.random() * 3)];
-  const result = playRound("shears", computerSelection);
-  unserWindow.innerHTML = "shears";
-  compWindow.innerHTML = computerSelection;
-  if (result === "win") {
-    userCountDisplay.innerHTML = parseInt(userCountDisplay.innerHTML) + 1;
-  } else if (result === "lose") {
-    compCountDisplay.innerHTML = parseInt(compCountDisplay.innerHTML) + 1;
-  }
-});
+}
+const clickStone = () => {
+  makeSelection("stone");
+};
+const clickPaper = () => {
+  makeSelection("paper");
+};
+const clickShears = () => {
+  makeSelection("shears");
+};
 
 restartButton.addEventListener("click", function () {
   location.reload();

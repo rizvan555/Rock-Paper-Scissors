c128c68976bba92f3e8309410c45e16f70ed38d6
Rizvan555 <rizvan84@gmx.de>
2023-03-18 18:19:37 +0100

    changed titles

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,4 +1,4 @@
-const unserWindow = document.querySelector(".user-window");
+const userWindow = document.querySelector(".user-window");
 const compWindow = document.querySelector(".comp-window");
 const count5 = document.querySelector(".count-5");
 const count10 = document.querySelector(".count-10");
@@ -14,34 +14,31 @@ const restartButton = document.querySelector(".restart");
 
 let playerScore = 0;
 let computerScore = 0;
+let round = 0;
 const moves = ["stone", "paper", "shears"];
-unserWindow.innerHTML = playerScore;
+userWindow.innerHTML = playerScore;
 compWindow.innerHTML = computerScore;
 
-function playRound(playerSelection, computerSelection) {
-  for (let index = 0; index <= 5; index++) {
-    if (playerSelection === computerSelection) {
-      return "tie";
-    } else if (
-      (playerSelection === "stone" && computerSelection === "shears") ||
-      (playerSelection === "paper" && computerSelection === "stone") ||
-      (playerSelection === "shears" && computerSelection === "paper")
-    ) {
-      playerScore++;
-      return "win";
-    } else {
-      computerScore++;
-      return "lose";
-    }
+function playGame(playerSelection, computerSelection) {
+  if (playerSelection === computerSelection) {
+    return "tie";
+  } else if (
+    (playerSelection === "stone" && computerSelection === "shears") ||
+    (playerSelection === "paper" && computerSelection === "stone") ||
+    (playerSelection === "shears" && computerSelection === "paper")
+  ) {
+    playerScore++;
+    return "win";
+  } else {
+    computerScore++;
+    return "lose";
   }
 }
 
-let round = 0;
-
 function makeSelection(selection) {
   const computerSelection = moves[Math.floor(Math.random() * 3)];
-  const result = playRound(selection, computerSelection);
-  unserWindow.innerHTML = selection;
+  const result = playGame(selection, computerSelection);
+  userWindow.innerHTML = selection;
   compWindow.innerHTML = computerSelection;
   round++;
 
@@ -52,17 +49,6 @@ function makeSelection(selection) {
   }
 
   if (round === 5) {
-    if (playerScore > computerScore) {
-      startCommand.innerHTML = "You win!";
-    } else if (computerScore > playerScore) {
-      startCommand.innerHTML = "Computer wins!";
-    } else {
-      startCommand.innerHTML = "It's a tie!";
-    }
-    stoneButton.disabled = true;
-    paperButton.disabled = true;
-    shearsButton.disabled = true;
-  } else if (round === 10) {
     if (playerScore > computerScore) {
       startCommand.innerHTML = "You win!";
     } else if (computerScore > playerScore) {
